049e7c6b3660b21d5561280238cbd18efa345dd3
Roger Padilla <rpadilla@velocityparnerts.net>
2017-08-13 13:17:34 -0500

    working with js api

--- src/toolbar/toolbar.ts
+++ src/toolbar/toolbar.ts
@@ -20,8 +20,7 @@ function addToolbar() {
   const omitNonWorking = toolbar.querySelector('.adp-next__omit-non-working') as HTMLInputElement;
 
   copy.addEventListener('click', () => {
-    // copyAll();
-    newCopyAll();
+    copyAll();
   });
 
   whenElementReady('TcGrid', () => {
@@ -31,85 +30,68 @@ function addToolbar() {
 }
 
 function copyAll() {
-  // busy(true);
-  // const length = obtainRows().length;
-  // copyRows(0, length, () => {
-  //   deleteNonWorkingRows(0, length, () => {
-  //     busy(false);
-  //   });
-  // });
-}
-
-function newCopyAll() {
 
   const ONE_DAY = 864e5;
   const rows = TcGridView.ProcessedServerResponse.items;
-  const n = rows.length - 2;
+  const n = rows.length - 1;
+  let srcIdx = 1;
 
-  for (let idx = 1; idx < n; idx++) {
-    const days = ONE_DAY;
-    const pos = TcGridUtil.StoreItemLocation(rows, idx);
-    const row = rows[pos];
-    const srcRow = dojo.clone(row);
-    
-    srcRow.PayDate = TLMJS.IsDate(srcRow.PayDate) ? new Date(srcRow.PayDate.valueOf() + days) : undefined;
-    srcRow.InDate = TLMJS.IsDate(srcRow.InDate) ? new Date(srcRow.InDate.valueOf() + days) : undefined;
-    srcRow.OutDate = TLMJS.IsDate(srcRow.OutDate) ? new Date(srcRow.OutDate.valueOf() + days) : undefined;
+  let srcRow = rows[srcIdx];
+  let isWorkingDate = checkIsWorkingDate(new Date(srcRow.InDate.valueOf()));
 
-    let newPos = pos + 1;
-    const v = checkIsWorkingDate(srcRow.InDate);
+  while (!isWorkingDate && srcIdx < n) {
+    srcRow = rows[srcIdx];
+    srcIdx++;
+    isWorkingDate = checkIsWorkingDate(new Date(srcRow.InDate.valueOf()));
+  }
 
-    if (!v) {
-      idx += 2;
-      newPos += 2;
+  let plusDays = 1;
+  let newIdx = srcIdx + 1;
+
+  while (newIdx < n) {
+
+    let nextRow = rows[newIdx];
+    isWorkingDate = checkIsWorkingDate(new Date(nextRow.InDate.valueOf()));    
+
+    let c = 0;
+
+    while (!isWorkingDate && newIdx < n) {
+      plusDays++;
+      newIdx++;
+      nextRow = rows[newIdx];
+      isWorkingDate = checkIsWorkingDate(new Date(nextRow.InDate.valueOf()));
+      c = 2;
     }
 
+    if (c) {
+      plusDays -= c;
+    }
 
-    console.log('* idx', idx);
-    console.log('* pos', pos);
+    const days = ONE_DAY * plusDays;
+
+    const newRow = dojo.clone(srcRow);
+    newRow.PayDate = TLMJS.IsDate(newRow.PayDate) ? new Date(newRow.PayDate.valueOf() + days) : undefined;
+    newRow.InDate = TLMJS.IsDate(newRow.InDate) ? new Date(srcRow.InDate.valueOf() + days) : undefined;
+    newRow.OutDate = TLMJS.IsDate(newRow.OutDate) ? new Date(newRow.OutDate.valueOf() + days) : undefined;
+
+    const newPos = TcGridUtil.StoreItemLocation(rows, newIdx);
+
+    console.log('* idx', plusDays);
+    console.log('* newIdx', newIdx);
     console.log('* newPos', newPos);
-    console.log('* row', row);
-    console.log('* srcRow', srcRow);
 
-    const newRow = TcGridUtil.CopyRow(srcRow, !0, !0);
-    newRow.WeekNumber = TcGridUtil.IdentifyWeekNumber(newRow.InDate);
-    // const insertPosition = TcGridUtil.StoreItemLocation(rows, 1) + 1;
-    // rows.splice(newPos, 0, newRow);
-    rows[newPos] = newRow;
+    const newCopiedRow = TcGridUtil.CopyRow(newRow, true, true);
+    newCopiedRow.WeekNumber = TcGridUtil.IdentifyWeekNumber(newCopiedRow.InDate);
+    rows[newPos] = newCopiedRow;
+
+    plusDays++;
+    newIdx++;
   }
 
   TcGridUtil.ResetStoreArrayPositions();
   TcGridUtil.CreateDTORecordXReferences();
   dojo.destroy(TcGridTable.DataGrid.grid);
   TcGridUtil.renderNewAndAdjustLayout();
-}
-
-function copyRows(idx: number, length: number, callback?: Function) {
-
-  if (idx >= length) {
-    if (callback) {
-      callback();
-    }
-    return;
-  }
-
-  const row = obtainRow(idx);
-  const isFilled = checkIfRowIsFilled(row);
-  if (!isFilled) {
-    const alert = document.querySelector('#alert') as any;
-    alert.querySelector('.adp-next__row-number').textContent = idx + 1;
-    alert.querySelector('.adp-next__close').onclick = () => {
-      alert.close();
-    };
-    alert.showModal();
-    return;
-  }
-
-  executeRowAction(row, 'Copy Row to Next Day');
-
-  setTimeout(() => {
-    copyRows(idx + 1, length, callback);
-  }, 0);
 }
 
 function busy(show: boolean) {
@@ -129,83 +111,6 @@ function busy(show: boolean) {
     grid.removeChild(busyBox);
     copy.removeAttribute('disabled');
     omitNonWorking.removeAttribute('disabled');
-  }
-}
-
-function deleteNonWorkingRows(idx: number, length: number, callback?: Function) {
-
-  if (idx >= length) {
-    if (callback) {
-      callback(length);
-    }
-    return;
-  }
-
-  const row = obtainRow(idx);
-  const isWorking = checkIsWorkingRow(row);
-
-  if (isWorking) {
-    idx++;
-  } else {
-    executeRowAction(row, 'Delete Row');
-    length--;
-  }
-
-  setTimeout(() => {
-    deleteNonWorkingRows(idx, length, callback);
-  }, 0);
-}
-
-function executeRowAction(row: HTMLElement, commandText: string) {
-
-  const command = commandText.toLocaleLowerCase();
-
-  const menuIcon = row.querySelector('.menuIcon') as HTMLElement;
-  menuIcon.click();
-  const menu = Array.from(document.querySelectorAll('.dijitPopup') as NodeListOf<HTMLElement>)
-    .find(it => it.style.display !== 'none');
-  if (!menu) {
-    console.warn('Menu not found', row);
-    return;
-  }
-
-  const items = Array.from(menu.querySelectorAll('.dijitMenuItemLabel') as NodeListOf<HTMLElement>);
-  const actionItem = items.find(it => it.textContent.trim().toLowerCase() === command);
-  if (!actionItem) {
-    console.warn('Action not found', menu, command);
-    return;
-  }
-
-  actionItem.click();
-}
-
-function checkIsWorkingRow(row: HTMLElement) {
-  const year = new Date().getFullYear();
-  const dateStr = row.querySelector('.Editable[id$=_InDate]').textContent;
-  const dateArr = dateStr.split('/');
-  const month = +dateArr[0] - 1;
-  const dateOfMonth = +dateArr[1];
-  const date = new Date(year, month, dateOfMonth);
-  return checkIsWorkingDate(date);
-}
-
-function obtainRows() {
-  return Array.from(document.querySelectorAll('#TcGrid .dR') as NodeListOf<HTMLElement>);
-}
-
-function obtainRow(idx = 0) {
-  const rows = obtainRows();
-  return rows[idx];
-}
-
-function checkIfRowIsFilled(row: HTMLElement) {
-  const fields = Array.from(row.querySelectorAll('.Editable'));
-  for (const field of fields) {
-    if (!field.id.endsWith('_InDate')
-      && !field.id.endsWith('_Value')
-      && field.textContent.trim()) {
-      return true;
-    }
   }
 }
 
