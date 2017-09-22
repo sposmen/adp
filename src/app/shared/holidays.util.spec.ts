60d77e2d8b66addffec563d0f69c0f937c32482e
Roger Padilla <rpadilla@velocityparnerts.net>
2017-09-22 11:21:03 -0500

    Use dates with hours, mins and secs for tests

--- src/app/shared/holidays.util.spec.ts
+++ src/app/shared/holidays.util.spec.ts
@@ -10,34 +10,34 @@ describe('holidays.util', () => {
 
   it('holidays AR', () => {
     holidaysUtil.init('AR');
-    expect(holidaysUtil.isHoliday(new Date(2017, 6, 19))).toBe(false);
-    expect(holidaysUtil.isHoliday(new Date(2017, 1, 27))).toBeTruthy();
-    expect(holidaysUtil.isHoliday(new Date(2017, 11, 8))).toBeTruthy();
-    expect(holidaysUtil.isHoliday(new Date(2017, 11, 25))).toBeTruthy();
-    expect(holidaysUtil.isHoliday(new Date(2018, 0, 1))).toBeTruthy();
-    expect(holidaysUtil.isHoliday(new Date(2018, 0, 2))).toBe(false);
+    expect(holidaysUtil.isHoliday(new Date(2017, 6, 19, 0, 0, 0))).toBe(false);
+    expect(holidaysUtil.isHoliday(new Date(2017, 1, 27, 0, 0, 0))).toBeTruthy();
+    expect(holidaysUtil.isHoliday(new Date(2017, 11, 8, 0, 0, 0))).toBeTruthy();
+    expect(holidaysUtil.isHoliday(new Date(2017, 11, 25, 0, 0, 0))).toBeTruthy();
+    expect(holidaysUtil.isHoliday(new Date(2018, 0, 1, 0, 0, 0))).toBeTruthy();
+    expect(holidaysUtil.isHoliday(new Date(2018, 0, 2, 0, 0, 0))).toBe(false);
   });
 
   it('holidays CO', () => {
     holidaysUtil.init('CO');
-    expect(holidaysUtil.isHoliday(new Date(2017, 6, 19))).toBe(false);
-    expect(holidaysUtil.isHoliday(new Date(2017, 6, 20))).toBeTruthy();
-    expect(holidaysUtil.isHoliday(new Date(2017, 11, 8))).toBeTruthy();
-    expect(holidaysUtil.isHoliday(new Date(2017, 11, 25))).toBeTruthy();
-    expect(holidaysUtil.isHoliday(new Date(2018, 0, 1))).toBeTruthy();
-    expect(holidaysUtil.isHoliday(new Date(2018, 0, 2))).toBe(false);
+    expect(holidaysUtil.isHoliday(new Date(2017, 6, 19, 0, 0, 0))).toBe(false);
+    expect(holidaysUtil.isHoliday(new Date(2017, 6, 20, 0, 0, 0))).toBeTruthy();
+    expect(holidaysUtil.isHoliday(new Date(2017, 11, 8, 0, 0, 0))).toBeTruthy();
+    expect(holidaysUtil.isHoliday(new Date(2017, 11, 25, 0, 0, 0))).toBeTruthy();
+    expect(holidaysUtil.isHoliday(new Date(2018, 0, 1, 0, 0, 0))).toBeTruthy();
+    expect(holidaysUtil.isHoliday(new Date(2018, 0, 2, 0, 0, 0))).toBe(false);
   });
 
   it('weekdays', () => {
-    expect(holidaysUtil.isWeekday(new Date(2017, 6, 14))).toBe(true);
-    expect(holidaysUtil.isWeekday(new Date(2017, 6, 15))).toBe(false);
-    expect(holidaysUtil.isWeekday(new Date(2017, 6, 16))).toBe(false);
-    expect(holidaysUtil.isWeekday(new Date(2017, 6, 20))).toBe(true);
-    expect(holidaysUtil.isWeekday(new Date(2017, 11, 8))).toBe(true);
-    expect(holidaysUtil.isWeekday(new Date(2017, 11, 25))).toBe(true);
-    expect(holidaysUtil.isWeekday(new Date(2017, 11, 30))).toBe(false);
-    expect(holidaysUtil.isWeekday(new Date(2017, 11, 31))).toBe(false);
-    expect(holidaysUtil.isWeekday(new Date(2018, 0, 1))).toBe(true);
+    expect(holidaysUtil.isWeekday(new Date(2017, 6, 14, 0, 0, 0))).toBe(true);
+    expect(holidaysUtil.isWeekday(new Date(2017, 6, 15, 0, 0, 0))).toBe(false);
+    expect(holidaysUtil.isWeekday(new Date(2017, 6, 16, 0, 0, 0))).toBe(false);
+    expect(holidaysUtil.isWeekday(new Date(2017, 6, 20, 0, 0, 0))).toBe(true);
+    expect(holidaysUtil.isWeekday(new Date(2017, 11, 8, 0, 0, 0))).toBe(true);
+    expect(holidaysUtil.isWeekday(new Date(2017, 11, 25, 0, 0, 0))).toBe(true);
+    expect(holidaysUtil.isWeekday(new Date(2017, 11, 30, 0, 0, 0))).toBe(false);
+    expect(holidaysUtil.isWeekday(new Date(2017, 11, 31, 0, 0, 0))).toBe(false);
+    expect(holidaysUtil.isWeekday(new Date(2018, 0, 1, 0, 0, 0))).toBe(true);
   });
 
 });
